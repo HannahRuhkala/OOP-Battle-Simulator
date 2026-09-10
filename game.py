5e5f5c2c42807f671aafbb1bacc2016e9b8cd3b9
HannahRuhkala <hannahruhkala@gmail.com>
2026-09-09 21:54:56 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Circle"
+ARENA_NAME = "The Colusseum of Chaos"
 
 
 def main():
@@ -10,10 +10,10 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("Cornilius")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    print("But no hero has answered the call... yet.")
+    print("Will he play the game of death to live or die to give another life.")
 
 
 if __name__ == "__main__":
